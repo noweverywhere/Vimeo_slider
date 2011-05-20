53a085d398b04704cfe1178a8783af0bcf2298b3
Marinus Swanepoel <marinus@noweverywhere.ca>
2011-05-20 10:49:31 -0400

    getPage.php makes cURL call to Vimeo using the Vimeo Simple API v.2 and returns data to browser

---
+++ getPage.php
@@ -0,0 +1,31 @@
+<?php 
+$c = curl_init();
+
+curl_setopt($c, CURLOPT_URL, "http://vimeo.com/api/v2/fmenterprises/videos.json");		//url we want to load
+
+curl_setopt($c, CURLOPT_HEADER, false);		//true - return the HTTP headers as part of the text, false - NO headers
+
+curl_setopt($c, CURLOPT_RETURNTRANSFER, true);			//I want the page returned as a string	- if false then it returns 1 or 0
+
+$pagedata = curl_exec($c);
+
+$videosObj = json_decode($pagedata, true);
+$smallerObject;
+
+
+
+for ($i = 0; $i <= 19; $i++) {
+    $smallerObject[$i]['id'] = $videosObj[$i]['id'];
+	$smallerObject[$i]['thumbnail_large'] = $videosObj[$i]['thumbnail_large'];
+	
+}
+
+$AbreviatedVidData = json_encode($smallerObject);
+
+header("HTTP/1.1: 200");
+header("Content-Type: text/json");
+echo $AbreviatedVidData;
+
+
+curl_close($c);
+?>
