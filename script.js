0e23f74525904aeb246b592547fc3f200d62de25
Marinus Swanepoel <marinus@noweverywhere.ca>
2011-05-24 15:36:08 -0400

    when you click an image it is replaced with an embedded video that is formatted to the correct size

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ $(document).ready(init);
 var videoArray = [];
 var currentSlide = 0;
 var videoData = '';
-var vimeoEmbedP1 = '<iframe src="http://player.vimeo.com/video/'
-var vimeoEmbedP2 = '?title=0&amp;byline=0&amp;portrait=0" width="640" height="360" frameborder="0"></iframe>'
-
+var vimeoEmbedStr = '<iframe src="http://player.vimeo.com/video/XXX?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1" width="XXX" height="XXX" frameborder="0"></iframe>'
+var vimArr = vimeoEmbedStr.split('XXX');
 function init(){
 	//empty the unordered list to start with a clean slate 
-	$('#slider ul').empty() 
+	
+	
 	
 	//bind the button actions
 	$('#plus').bind('click',plus); 
@@ -23,42 +23,52 @@ function init(){
 }
 
 function gotVideos(stuff){
-	numberOfVids = stuff.length //store value to speed up loop
+	$('#slider ul').empty;
+	//store value to speed up loop
+	numberOfVids = stuff.length 
 	for(i = 0; i< numberOfVids; i++){
 		//store the id and thumbnail adresses for easy handling
 		id = stuff[i].id;
 		thumb = stuff[i].thumbnail_large;
-		$('#slider ul').append('<li class="vimeoThumb" ><img class="'+id+' banana" src="'+thumb+'"/></li>');
+		$('#slider ul').append('<li class="vimeoThumb" ><img class="vimeo_'+id+'" src="'+thumb+'"/></li>');
+	/*	$('.vimeo_'+id).load(function{ want to preload the images but will do that later
+			$('#slider ul').width(+=)
+		});*/
 	}
-	$('#slider ul li').bind('click',playVdeo);
-	
-	//[0-9]{8}
+	$('#slider').bind('click',playVdeo);
 
-	
 }
 
 function playVdeo(clickEvent){
-		console.log(clickEvent)
-		console.log(clickEvent.target.classList)
-		var classes = clickEvent.target.classList
-		for(n=0;n<classes.length;n++){
-			if(classes[n].match([0-9]{8})){
-				clickEvent.
-			}
+
+	idNumber = "";
+	clickEvent.target
+	//console.log(clickEvent)
+	var classes = clickEvent.target.classList
+	for(n=0;n<classes.length;n++){
+		if(classes[n].match( /vimeo_[0-9]{8}/)){
+			thisClass = classes[n];
+			idNumber = thisClass.substr(thisClass.indexOf('_')+1);
+			//console.log('idNumber > '+ idNumber)
+			embedVideo(idNumber)
 		}
-		console.log(clickEvent.target.className)
-		//console.log(this.attr("class"))
-		//target.attr('class')
 	}
+	
+	function embedVideo(idNumber){
+		height = $('.vimeo_'+idNumber).height();
+		width = $('.vimeo_'+idNumber).width();
+		$('.vimeo_'+idNumber).replaceWith(vimArr[0]+idNumber+vimArr[1]+width+vimArr[2]+height+vimArr[3]);
+	}
+}
 
 function plus(){
 	currentSlide++
-	if(currentSlide > 20){currentSlide = 0}
+	if(currentSlide > 19){currentSlide = 0}
 	console.log('next > '+ currentSlide)
 }
 
 function minus(){
 	currentSlide--
-	if(currentSlide < 0){currentSlide = 20}
+	if(currentSlide < 0){currentSlide = 19}
 	console.log('back > '+ currentSlide)
 }
